2f74068367619adbd7f274af44188fbd7be6187e
eric_ <eric_@DESKTOP-E06PVB6>
2020-02-24 01:31:51 -0500

    Version 2

--- src/TrigFunctions/TrigFunctions.java
+++ src/TrigFunctions/TrigFunctions.java
@@ -1,4 +1,31 @@
+/*
+Eric Yeung
+104392784
+
+Creating trig functions from scratch
+Sin function was created from the taylor series
+Cos and Tan functions were created from trig identities
+ */
+
 package TrigFunctions;
 
-public class TrigFunctions {
+public class TrigFunctions
+{
+    public static final double PI = 3.14159265359;
+
+    //Method to convert degrees to radians
+    public double toRadians(double deg)
+    {
+        double value = deg * (PI / 180);
+        return value;
+    }
+
+    //Method to convert radians to degrees
+    public double toDegrees(double rad)
+    {
+        double value = rad * (180 / PI);
+        return value;
+    }
+
+
 }
